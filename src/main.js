0e66d7aae813fc0f134e64d03f00520e6d2d5e28
cade-kd <cade@cade.cool>
2021-12-14 22:28:09 +0800

    feat mock gendata

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
-import router from '@/router'
+Vue.config.productionTip = false;
+import router from "@/router";
+import store from "@/store";
+
+import "@/mock/mockServe";
 
 new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+  store,
+}).$mount("#app");
---
+++ src/store/search/index.js
@@ -0,0 +1,15 @@
+//search 小仓库
+const state = {};
+
+const mutasearchtions = {};
+
+const actions = {};
+
+const getters = {};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
